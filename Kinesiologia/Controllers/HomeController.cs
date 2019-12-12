ffd8b0af191d6261ef88946f502d349b8db1cb2d
development6 <40081976+development6@users.noreply.github.com>
2019-12-12 02:01:21 -0300

    commit 4

--- Kinesiologia/Controllers/HomeController.cs
+++ Kinesiologia/Controllers/HomeController.cs
@@ -76,5 +76,104 @@ namespace Kinesiologia.Controllers
 
 
 
+        [HttpPost]
+        public ContentResult AjaxMethod2(string genero, string riesgo, string fecha)
+        {
+            //string query = "select AVG(Ficha_Kinesiologia.ergo_vol_ing), AVG(Ficha_Kinesiologia.ergo_voml_ing), AVG(Ficha_Kinesiologia.ergo_vol_egr), AVG(Ficha_Kinesiologia.ergo_voml_egr)";
+            //query += "FROM Ficha INNER JOIN Ficha_Kinesiologia On Ficha_Kinesiologia.id_ficha=Ficha.id_ficha INNER JOIN Paciente on Paciente.id_paciente = Ficha.id_paciente INNER JOIN Persona on Persona.id_persona = Paciente.id_persona where Ficha_Kinesiologia.riesgo = @Riesgo And Persona.sexo = @Genero and Ficha_Kinesiologia.ergo_fecha_egr = @Fecha group by Ficha_Kinesiologia.ergo_vol_ing, Ficha_Kinesiologia.ergo_voml_ing, Ficha_Kinesiologia.ergo_vol_ing, Ficha_Kinesiologia.ergo_voml_ing";
+
+            string query = "select Replace(AVG(Ficha_Kinesiologia.ergo_vol_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_voml_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_fcmax_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_pulso_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_ve_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_mets_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_vol_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_voml_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_fcmax_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_pulso_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_ve_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_mets_egr), ',', '.') From Ficha INNER JOIN Ficha_Kinesiologia On Ficha_Kinesiologia.id_ficha = Ficha.id_ficha INNER JOIN Paciente on Paciente.id_paciente = Ficha.id_paciente INNER JOIN Persona on Persona.id_persona = Paciente.id_persona where Persona.sexo = @Genero1 And Ficha_Kinesiologia.riesgo=@Riesgo2 And ergo_fecha_ing BETWEEN @Fecha1 And GETDATE()";
+
+            string constr = ConfigurationManager.ConnectionStrings["ConexionKaplan"].ConnectionString;
+            StringBuilder sb = new StringBuilder();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+
+                    cmd.Parameters.AddWithValue("@Genero1", genero);
+                    cmd.Parameters.AddWithValue("@Riesgo2", riesgo);
+                    cmd.Parameters.AddWithValue("@Fecha1", fecha);
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sb.Append("[");
+
+                        while (sdr.Read())
+                        {
+                            sb.Append("{");
+                            System.Threading.Thread.Sleep(50);
+                            string color = String.Format("#{0:X6}", new Random().Next(0x1000000));
+                            sb.Append(string.Format("value0:{0},value1:{1},value2:{2},value3:{3},value4:{4},value5:{5},value6:{6},value7:{7},value8:{8},value9:{9},value10:{10},value11:{11}", sdr[0], sdr[1], sdr[2], sdr[3], sdr[4], sdr[5], sdr[6], sdr[7], sdr[8], sdr[9], sdr[10], sdr[11]));
+                            sb.Append("},");
+                        }
+
+                        sb = sb.Remove(sb.Length - 1, 1);
+                        sb.Append("]");
+
+
+                        //sb.Append("}");
+                    }
+
+                    con.Close();
+                }
+            }
+
+            return Content(sb.ToString());
+        }
+
+
+
+
+        [HttpPost]
+        public ContentResult AjaxMethod3(string riesgo1)
+        {
+            //string query = "select AVG(Ficha_Kinesiologia.ergo_vol_ing), AVG(Ficha_Kinesiologia.ergo_voml_ing), AVG(Ficha_Kinesiologia.ergo_vol_egr), AVG(Ficha_Kinesiologia.ergo_voml_egr)";
+            //query += "FROM Ficha INNER JOIN Ficha_Kinesiologia On Ficha_Kinesiologia.id_ficha=Ficha.id_ficha INNER JOIN Paciente on Paciente.id_paciente = Ficha.id_paciente INNER JOIN Persona on Persona.id_persona = Paciente.id_persona where Ficha_Kinesiologia.riesgo = @Riesgo And Persona.sexo = @Genero and Ficha_Kinesiologia.ergo_fecha_egr = @Fecha group by Ficha_Kinesiologia.ergo_vol_ing, Ficha_Kinesiologia.ergo_voml_ing, Ficha_Kinesiologia.ergo_vol_ing, Ficha_Kinesiologia.ergo_voml_ing";
+
+            string query = "select Replace(AVG(Ficha_Kinesiologia.ergo_vol_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_voml_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_fcmax_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_pulso_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_ve_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_mets_ing), ',' , '.'), Replace(AVG(Ficha_Kinesiologia.ergo_vol_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_voml_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_fcmax_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_pulso_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_ve_egr), ',', '.'), Replace(AVG(Ficha_Kinesiologia.ergo_mets_egr), ',', '.') From Ficha_Kinesiologia where Ficha_Kinesiologia.riesgo = 'no'";
+
+            string constr = ConfigurationManager.ConnectionStrings["ConexionKaplan"].ConnectionString;
+            StringBuilder sb = new StringBuilder();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+
+                    //cmd.Parameters.AddWithValue("no", riesgo1);
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sb.Append("[");
+
+                        while (sdr.Read())
+                        {
+                            sb.Append("{");
+                            System.Threading.Thread.Sleep(50);
+                            string color = String.Format("#{0:X6}", new Random().Next(0x1000000));
+                            sb.Append(string.Format("value0:{0},value1:{1},value2:{2},value3:{3},value4:{4},value5:{5},value6:{6},value7:{7},value8:{8},value9:{9},value10:{10},value11:{11}", sdr[0], sdr[1], sdr[2], sdr[3], sdr[4], sdr[5], sdr[6], sdr[7], sdr[8], sdr[9], sdr[10], sdr[11]));
+                            sb.Append("},");
+                        }
+
+                        sb = sb.Remove(sb.Length - 1, 1);
+                        sb.Append("]");
+
+
+                        //sb.Append("}");
+                    }
+
+                    con.Close();
+                }
+            }
+
+            return Content(sb.ToString());
+        }
+
+
+
     }
 }
